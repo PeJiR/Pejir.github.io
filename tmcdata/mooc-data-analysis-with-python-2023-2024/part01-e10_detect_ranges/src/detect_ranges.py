57a3daa31ca38126442ff9e1f21b1bdbd15c0282
pejir <pejir84@gmail.com>
2024-05-04 01:40:27 +0200

    update

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e10_detect_ranges/src/detect_ranges.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
 
 def detect_ranges(L):
-    return []
+    L_sorted = sorted(L)
+    result = []
+    i=0
+    while L_sorted:
+        if len(L_sorted) ==1:
+            result.append(L_sorted[0])
+            L_sorted.pop(0)
+        elif L_sorted[i+1] == L_sorted[i]+1:
+            i+=1
+            if i == len(L_sorted)-1:
+                temp = (L_sorted[0],L_sorted[i]+1)
+                L_sorted = []
+                result.append(temp)
+        else:
+            if i == 0:
+                result.append(L_sorted[0])
+                L_sorted.pop(0)
+            else:
+                temp = (L_sorted[0], L_sorted[i]+1)
+                del L_sorted[:i+1]
+                result.append(temp)
+                i =0
+        return result
+                          
+    
+    
+    
+    
+    
+    
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
