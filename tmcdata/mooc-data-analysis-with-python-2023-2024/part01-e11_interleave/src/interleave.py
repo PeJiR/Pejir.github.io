a8de1dbd74be420bbf4631a34bdf2c64ba828bcb
pejir <pejir84@gmail.com>
2024-05-05 21:26:06 +0200

    a

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e11_interleave/src/interleave.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e11_interleave/src/interleave.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
+    """
+    Interleave elements from multiple lists.
+
+    Args:
+        *lists (iterable): One or more iterables.
+
+    Returns:
+        list: A list with elements interleaved from the input lists.
+    """
+    # Initialize an empty list to store the interleaved elements
     interleave_list = []
+
+    # Use zip() function to interleave elements from multiple lists
     for x in zip(*lists):
+        # Extend the interleave_list with the interleaved elements
         interleave_list.extend(x)
-           
+
+    # Return the interleaved list
     return interleave_list
 
 def main():
