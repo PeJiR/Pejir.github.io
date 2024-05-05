3ab9e02c6220a3f2bd5fcc7fb94b547594e79c26
pejir <pejir84@gmail.com>
2024-05-05 02:28:33 +0200

    update

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e11_interleave/src/interleave.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e11_interleave/src/interleave.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+    interleave_list = []
+    for x in zip(*lists):
+        interleave_list.extend(x)
+           
+    return interleave_list
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
