4e9b48a9f9df0e18c46798ce794309b54f84ca3c
pejir <pejir84@gmail.com>
2024-05-08 21:11:47 +0200

    update

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e20_usemodule/src/usemodule.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e20_usemodule/src/usemodule.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-# Don't modify the below hack
-try:
-    from src import triangle
-except ModuleNotFoundError:
-    import triangle
+from src import triangle
+ 
 
 def main():
+    """
+    This is the main function of the usemodule module.
+
+    It calls the hypotenuse and area functions from the triangle module
+    and prints their results.
+    """
+
     # Call the functions from here
+    print(triangle.hypotenuse(10, 20))  # Print the length of the hypotenuse of a right-angled triangle
+    print(triangle.area(10, 20))  # Print the area of a right-angled triangle
 
 if __name__ == "__main__":
     main()
