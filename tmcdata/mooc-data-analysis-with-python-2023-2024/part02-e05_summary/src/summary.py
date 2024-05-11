08fffe8cae4f9d6737f252c9df12ce27d8094cca
pejir <pejir84@gmail.com>
2024-05-12 01:17:41 +0200

    update

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e05_summary/src/summary.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e05_summary/src/summary.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+import math
 
 def summary(filename):
+    with open(filename, "r") as file:
+        summary_file = file.read()
+        
+        # Find the numeric values into a file
+        regex = re.findall(r"([+|-]*\d+.?\d*)", summary_file)
+        
+        # Convert to floats to calculate the total, average and standard deviation
+        
+        
+    
+    
     return (0,0,0)
 
 def main():
