a60c4a7cc08bacf6000cf8363412ac234212a57f
pejir <pejir84@gmail.com>
2024-05-12 02:19:34 +0200

    Update file_count.py

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e06_file_count/src/file_count.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e06_file_count/src/file_count.py
@@ -3,7 +3,19 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    lines = 0
+    words = 0
+    chars = 0
+    
+    with open(filename, "r") as file:
+        # loop through each line and start counting 
+        for line in file:
+            lines += 1
+            words += len(line.split())
+            chars += len(line)     
+    
+    
+    return (lines, words, chars)
 
 def main():
     pass
