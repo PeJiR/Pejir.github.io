0fb5cc9565bc2d5d85d2f37b9425f3e1ba046d8d
pejir <pejir84@gmail.com>
2024-05-12 03:14:35 +0200

    update

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e06_file_count/src/file_count.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e06_file_count/src/file_count.py
@@ -3,22 +3,30 @@
 import sys
 
 def file_count(filename):
-    lines = 0
-    words = 0
-    chars = 0
-    
+    """
+    Count the number of lines, words and characters in a file.
+
+    This function reads a file and counts the number of lines, words
+    and characters. The words are divided by splitting the text at
+    whitespace. The numbers are returned as a tuple.
+
+    :param filename: The name of the file to read.
+    :return: A tuple containing the number of lines, words and characters.
+    """
     with open(filename, "r") as file:
-        # loop through each line and start counting 
-        for line in file:
-            lines += 1
-            words += len(line.split())
-            chars += len(line)     
-    
-    
-    return (lines, words, chars)
+        content = file.read()
+        chars =  len(content)
+        words =  len(content.split())
+        lines =  content.count('\n')  
+        
+    return lines, words, chars
 
 def main():
-    pass
+     file_list = sys.argv[1:]
+     
+     for i in file_list:
+         lines, words, chars = file_count(i)
+         print(f"{lines}\t{words}\t{chars}\t{i}")
 
 if __name__ == "__main__":
     main()
