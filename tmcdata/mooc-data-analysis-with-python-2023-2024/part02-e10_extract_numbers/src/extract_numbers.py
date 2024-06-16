7998a57559c1c31e9c5824ddbee3a30389110253
Roberto Perez <129693538+PeJiR@users.noreply.github.com>
2024-06-16 02:29:03 +0200

    Update extract_numbers.py

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e10_extract_numbers/src/extract_numbers.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e10_extract_numbers/src/extract_numbers.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
 def extract_numbers(s):
-    return []
+    """
+    Extracts all integers and floats from a string and returns them as a list.
+    """
+    numbers = []
+    words = s.split()
+
+    for word in words:
+        try:
+            numbers.append(int(word))
+        except ValueError:
+            try:
+                numbers.append(float(word))
+            except ValueError:
+                pass
+
+    return numbers
 
 def main():
     print(extract_numbers("abd 123 1.2 test 13.2 -1"))
